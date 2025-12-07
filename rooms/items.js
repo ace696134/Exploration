b3996940401bd92405423a6e05cbea9b3478caec
ZaneDaBirb <acefrankel@yahoo.com>
2025-12-07 18:42:35 -0500

    Update items.js

--- rooms/items.js
+++ rooms/items.js
@@ -1,31 +1,65 @@
 /* -------- GLOBAL ITEM DEFINITIONS -------- */
 /* These are NOT what the player owns — these are just the item *templates*. */
+/*
+Common: #A3A3A3
+Uncommon: #35D413
+Rare: #13469C
+Mythical: #C713D4
+Legendary: #FDFF30
+*/
 
 window.ITEMS = {
   "Silver Key": {
     id: "silver_key",
     name: "Silver Key",
     icon: "../images/silver-key.png",
-    color: "#c0c0c0",
+    color: "#13469C",
     description: "A small metallic key. It looks old.",
+    stack: 16
+  },
+
+  "Key Mold": {
+    id: "key_mold",
+    name: "Key Mold",
+    icon: "../images/key_mold.png",
+    color: "#13469C",
+    description: "A small mold.. You could probably make a key with this.",
     stack: 1
   },
 
-  "Rusty Knife": {
-    id: "rusty_knife",
-    name: "Rusty Knife",
-    icon: "../images/rusty-knife.png",
-    color: "#a3472a",
-    description: "Covered in rust. Probably unsanitary.",
+  "Lighter": {
+    id: "lighter",
+    name: "Lighter",
+    icon: "../images/lighter.png",
+    color: "#35D413",
+    description: "A standard lighter with a print of fire under blue skies.",
+    stack: 1
+  },
+  
+  "Music Box": {
+    id: "music_box",
+    name: "Music Box",
+    icon: "../images/music_box.png",
+    color: "#13469C",
+    description: "A small music box playing a tune. You feel like something is attached to it..",
     stack: 1
   },
 
-  "Battery": {
-    id: "battery",
-    name: "Battery",
-    icon: "../images/battery.png",
-    color: "#ffee33",
-    description: "A standard AA battery.",
-    stack: 99
+    "Metal Scrap": {
+    id: "metal_scrap",
+    name: "Metal Scrap",
+    icon: "../images/metal_scrap.png",
+    color: "#A3A3A3",
+    description: "A small piece of metal scrap.. Could probably be melted and put into a mold if you had 5.",
+    stack: 128
+  },
+  
+    "Melted Metal Scrap": {
+    id: "melted_metal_scrap",
+    name: "Melted Metal Scrap",
+    icon: "../images/melted_metal_scrap.png",
+    color: "#A3A3A3",
+    description: "A small piece of metal scrap you melted.. Could probably be put into a mold if you had 5.",
+    stack: 5
   }
 };
